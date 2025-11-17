e774eef5472bf4582d6cb06875359ce38ee9f8af
Alberto Cabrera <betolcq@gmail.com>
2025-11-17 11:38:27 -0400

    se agrego un indice a la tabla de relacion para mejorar la optimizacion de las operacion, modificacion sugerida por el IDE, no se afecto el funcionamiento de la aplicacion, sigue funcionando correctamente como en las pruebas

--- parte02/GestionNotas/app/src/main/java/com/inf251/gestionnotas/data/entity/AsignacionEntity.kt
+++ parte02/GestionNotas/app/src/main/java/com/inf251/gestionnotas/data/entity/AsignacionEntity.kt
@@ -2,6 +2,7 @@ package com.inf251.gestionnotas.data.entity
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 
 
 @Entity(
@@ -11,6 +12,11 @@ import androidx.room.ForeignKey
         ForeignKey(entity = DocenteEntity::class, parentColumns = ["carnetDoc"], childColumns = ["ciDoc"]),
         ForeignKey(entity = MateriaEntity::class, parentColumns = ["siglaMat"], childColumns = ["sigMat"]),
         ForeignKey(entity = SemestreEntity::class, parentColumns = ["idSemestre"], childColumns = ["idSem"])
+    ],
+    indices = [
+        Index(value = ["ciDoc"]),
+        Index(value=["sigMat"]),
+        Index(value=["idSem"])
     ]
 )
 data class AsignacionEntity(
